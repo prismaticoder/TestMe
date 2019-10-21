d8b7993ce28a64c9afd1853ba11872731fe1f844
prismathic <pjjsalam1999@gmail.com>
2019-10-21 18:03:34 +0100

    Seeded values into classes tables and created tables for all subjects

---
+++ database/seeds/ClassesTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ClassesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        for ($i=1; $i < 4; $i++) {
+            DB::table('classes')->insert([
+                'id' => $i,
+                'class' => "JSS" . $i
+            ]);
+        }
+
+    }
+}
