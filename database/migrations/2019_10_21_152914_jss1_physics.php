5cc24683843edddec784bd5918784489a3ffa523
prismathic <pjjsalam1999@gmail.com>
2019-10-21 18:10:20 +0100

    DB config and seeder added

--- database/migrations/2019_10_21_152914_jss1_physics.php
+++ database/migrations/2019_10_21_152914_jss1_physics.php
@@ -13,15 +13,22 @@ class Jss1Physics extends Migration
      */
     public function up()
     {
+        $subjects = ['mathematics','english','rnv','basic_science','basic_tech','biology','chemistry','yoruba','ict','cca','french','phonics','music'];
         //Create the J1 Physics table
-        Schema::create('jss1_physics', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('class_id')->unsigned();
-            $table->foreign('class_id')->references('id')->on('classes');
-            $table->longText('question');
-            $table->json('options');
-            $table->string('answer');
-        });
+        for ($i=1; $i < 4; $i++) {
+            foreach ($subjects as $subject) {
+                Schema::create('jss'.$i.'_' .$subject, function (Blueprint $table) {
+                    $table->increments('id');
+                    $table->integer('class_id')->unsigned();
+                    $table->foreign('class_id')->references('id')->on('classes');
+                    $table->longText('question');
+                    $table->json('options');
+                    $table->string('answer');
+                });
+            }
+
+        }
+
     }
 
     /**
