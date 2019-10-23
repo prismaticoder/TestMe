87eb396eae89d032baa005f1a22a5b2ee04c21f4
prismathic <pjjsalam1999@gmail.com>
2019-10-23 15:51:41 +0100

    Changed user model to read from students table

--- app/User.php
+++ app/User.php
@@ -10,13 +10,15 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'students';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'firstname', 'lastname', 'class_id',
     ];
 
     /**
