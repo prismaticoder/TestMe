36a71be763dc98c1e955c32af33c6eacfe8f4513
prismathic <pjjsalam1999@gmail.com>
2019-10-25 00:03:19 +0100

    Revamped exam view with seeded variables

--- resources/views/exam.blade.php
+++ resources/views/exam.blade.php
@@ -1,6 +1,9 @@
 {{-- Welcome to our SPA. This is the view for the exam the student will be undertaking --}}
 @foreach ($questions as $question)
-    {{$question->question}}<br>
-    {{$question->options}}
+    <b>Question</b> {{$question->question}}<br>
+
+    @foreach ($question->options as $option)
+        Option: <pre>{{$option->body}}</pre>
+    @endforeach
 
 @endforeach
