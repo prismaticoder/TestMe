69b2b9c2acdaaaa8c0d425fd7c2a60aeed81cfe3
prismathic <pjjsalam1999@gmail.com>
2019-10-24 06:13:54 +0100

    Added views for dashboard, login and students page

---
+++ resources/views/admin/class-students.blade.php
@@ -0,0 +1,1 @@
+{{-- Page showing all students of each class with links to add, edit or delete the students --}}
---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,1 @@
+{{-- This is the view for the admin main page. It contains the list of all subjects with links to host exam, add questions, view results, it also has a navbar with links to the students page --}}
