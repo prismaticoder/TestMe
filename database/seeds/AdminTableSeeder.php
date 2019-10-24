70a8993bcaedf2c206c7cac4586ea19c391b4f33
prismathic <pjjsalam1999@gmail.com>
2019-10-24 08:24:34 +0100

    Seeded db for admin details and added questions view

---
+++ database/seeds/AdminTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'username' => 'Tomiwa',
+            'password' => '1111'
+        ]);
+    }
+}
---
+++ resources/views/admin/questions.blade.php
@@ -0,0 +1,1 @@
+{{-- This is where the admin adds questions for each subject, edits, deletes and views them. SUmmernote will be on this page --}}
