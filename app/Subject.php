73a6ce773928d332e41153271243dfb8a2cd75cd
prismathic <pjjsalam1999@gmail.com>
2019-10-24 11:28:10 +0100

    Edited subject model to retrieve questions and answers from it

--- app/Subject.php
+++ app/Subject.php
@@ -10,4 +10,12 @@ class Subject extends Model
     public function scores() {
         return $this->hasMany(Score::class);
     }
+
+    public function questions() {
+        return $this->hasMany(Question::class);
+    }
+
+    public function answers() {
+        return $this->hasManyThrough(Answer::class, Question::class);
+    }
 }
