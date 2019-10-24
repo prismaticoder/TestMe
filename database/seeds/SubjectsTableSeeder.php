992c828155e8fc422010e78b4f45d8986759d5be
prismathic <pjjsalam1999@gmail.com>
2019-10-24 18:45:40 +0100

    Added seeder markup for seeding the subjects table

---
+++ database/seeds/SubjectsTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class SubjectsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Seeder to input every subject into the db
+    }
+}
