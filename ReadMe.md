2d790d6e5f3caf45967cb5ec2b4e657b8a1966fe
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-25 17:44:08 +0100

    init

---
+++ ReadMe.md
@@ -0,0 +1,11 @@
+## ToDo
+1. Admin dashboard
+2. Admin Login Page
+3. Students Login Page
+4. Single Page for Showing each question with their options (there's a "next" button for the user to choose the next question)
+5. Page for viewing all students eligible to write an exam for a class
+6. Page that Lists all subjects with links to host the exam and also to add question, also to view all results of students who took the question
+7. Admin Interface for adding all questions with summernote wysiwyg plugin
+8. Submission Successful page for the student
+9. Host Exam Page for the admin to be able to end exam abruptly and to see the number of submissions and the number of people doing the exam per class
+10. Page to show results of all students in a certain subject
--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,13 +1,86 @@
-{{-- Admin Login Page --}}
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<form action="{{route('admin-login')}}" method="post">
-    @csrf
+        <title>Laravel</title>
 
-    @foreach ($error as $err)
-        {{$err}}
-    @endforeach<br>
-    Username: <input type="text" name="username"><br>
-    Password: <input type="password" name="password"><br>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-    <input type="submit" value="Submit Details">
-</form>
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ public_path('css/bootstrap.min.css') }}">
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+
+        </div>
+
+        {{-- Admin Login Page --}}
+
+        <form action="{{route('admin-login')}}" method="post">
+            @csrf
+
+            @foreach ($error as $err)
+                {{$err}}
+            @endforeach<br>
+            Username: <input type="text" name="username"><br>
+            Password: <input type="password" name="password"><br>
+
+            <input type="submit" value="Submit Details">
+        </form>
+    </body>
+</html>
