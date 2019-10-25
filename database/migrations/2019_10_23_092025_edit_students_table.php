4f9d95ef4922e622632e724901514d1d43575d55
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-25 17:15:37 +0100

    init

--- database/migrations/2019_10_23_092025_edit_students_table.php
+++ database/migrations/2019_10_23_092025_edit_students_table.php
@@ -13,7 +13,7 @@ class EditStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::table('students', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             $table->softDeletes();
         });
     }
@@ -25,7 +25,7 @@ class EditStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::table('students', function(Blueprint $table) {
+        Schema::table('users', function(Blueprint $table) {
             $table->dropColumn('deleted_at');
         });
 
