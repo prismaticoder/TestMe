432b27da7b5354c14e22c732637aab5296ea7706
prismathic <pjjsalam1999@gmail.com>
2019-10-26 17:14:36 +0100

    Added finesse to exam view

--- resources/views/exam.blade.php
+++ resources/views/exam.blade.php
@@ -1,11 +1,14 @@
 {{-- Welcome to our SPA. This is the view for the exam the student will be undertaking --}}
+@php $n = 1 @endphp
 @foreach ($questions as $question)
-    <b>Question</b> {{$question->question}}<br>
+    {{$n . '. '. $question->question}}<br>
 
     @php $letter = "A" @endphp
+    <ul>
     @foreach ($question->options as $option)
-        Option {{$letter}}: <pre>{{$option->body}}</pre>
+        <li> {{$letter}}: {{$option->body}}</li>
         @php $letter++ @endphp
     @endforeach
-
+    </ul>
+    @php $n++ @endphp
 @endforeach
