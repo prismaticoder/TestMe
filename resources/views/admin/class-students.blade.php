293f0565af4590a8ec9f4f9ac84b5d3282b38814
prismathic <pjjsalam1999@gmail.com>
2019-10-26 17:09:17 +0100

    Added dummy names for student view

--- resources/views/admin/class-students.blade.php
+++ resources/views/admin/class-students.blade.php
@@ -1,1 +1,7 @@
 {{-- Page showing all students of each class with links to add, edit or delete the students --}}
+<table>
+    <tr><th>Firstname</th><th>Lastname</th><th>Registration Number</th></tr>
+@foreach ($students as $student )
+    <tr><td>{{$student->firstname}}</td><td>{{$student->lastname}}</td><td>{{$student->code}}</td></tr>
+@endforeach
+<table>
