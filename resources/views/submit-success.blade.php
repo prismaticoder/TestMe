5243bfbc5ca0ff2e50d8745565fa6b712cfd77db
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-28 16:17:54 +0100

    fix

--- resources/views/submit-success.blade.php
+++ resources/views/submit-success.blade.php
@@ -142,7 +142,7 @@
     <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
   <header class="masthead mb-auto">
     <div class="inner">
-      <h3 class="masthead-brand">Cover</h3>
+      <h3 class="masthead-brand">One Time Schools</h3>
       <nav class="nav nav-masthead justify-content-center">
         <a class="nav-link active" href="#">Home</a>
         <a class="nav-link" href="#">Features</a>
