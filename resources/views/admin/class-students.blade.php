4ecf6e97cfbba86aa1b4e6e2d53b20ac18385574
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-29 17:32:50 +0100

    fix

--- resources/views/admin/class-students.blade.php
+++ resources/views/admin/class-students.blade.php
@@ -26,7 +26,7 @@
   <body>
 
     <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-    <a class="navbar-brand" href="{{route('dashboard')}}">Dashboard</a>
+    <a class="navbar-brand" href="{{route('dashboard')}}">One Time Schools</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
