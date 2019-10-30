cc254810b3ecb5e570eaecf7612e1b791b85b971
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-10-30 15:39:12 +0100

    FIX

--- resources/views/admin/class-students.blade.php
+++ resources/views/admin/class-students.blade.php
@@ -21,6 +21,10 @@
             margin-top: 120px;
             text-align: center;
         }
+        td button {
+          color: #fff;
+          background-color: blue;
+        }
     </style>
   </head>
   <body>
@@ -49,29 +53,73 @@
     </nav>
 
     <div class="container contents">
-    <h2>JSS{{$class->id}} Student List</h2>
+    <h2>JSS{{$class->id}} Student List <button href="#" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModalCenter">ADD STUDENT</button></h2>
 
           <table class="table table-s table-hover table-bordered">
               <thead class="thead-dark">
                 <tr>
-                  <th scope="col">Registration Number</th>
-                  <th scope="col">Firstname</th>
-                  <th scope="col">Lastname</th>
-                  <th scope="col">Class</th>
+                  <th>Registration Number</th>
+                  <th>Firstname</th>
+                  <th>Lastname</th>
+                  <th>Class</th>
+                  <th>Options</th>
                 </tr>
               </thead>
               <tbody>
                 @foreach ($students as $student )
                     <tr>
-                        <td scope="col">{{$student->code}}</td>
-                        <td scope="col">{{$student->firstname}}</td>
-                        <td scope="col">{{$student->lastname}}</td>
-                        <td scope="col">JSS{{$student->class_id}}</td>
+                        <td>{{$student->code}}</td>
+                        <td>{{$student->firstname}}</td>
+                        <td>{{$student->lastname}}</td>
+                        <td>JSS{{$student->class_id}}</td>
+                        <td >
+                          <button href="#" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#exampleModalCenter">EDIT</button>
+                          <button href="#" class="btn btn-secondary btn-sm">DELETE</button>
+                        </td>
                     </tr>
                 @endforeach
 
               </tbody>
             </table>
           </div>
+
+          <!-- Modal -->
+        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+              <form>
+                <div class="row">
+                <div class="col">
+                    <label for="">Time</label>
+                  </div>
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Hour">
+                  </div>
+                  <div class="col">
+                    <input type="text" class="form-control" placeholder="Minutes">
+                  </div>
+                </div>
+                <div class="form-group row mt-3">
+                  <label for="colFormLabel" class="col-sm-2 col-form-label">Score</label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="colFormLabel" placeholder="score">
+                  </div>
+                </div>
+              </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Start Exam</button>
+              </div>
+            </div>
+          </div>
+        </div>
     </body>
 </html>
