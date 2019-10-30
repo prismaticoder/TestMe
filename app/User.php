fdaab5acafe07357afcea5796cc1551a09bad338
prismathic <pjjsalam1999@gmail.com>
2019-10-30 17:27:08 +0100

    Seeded more users to the db

--- app/User.php
+++ app/User.php
@@ -24,16 +24,16 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    // protected $hidden = [
+    //     'password', 'remember_token',
+    // ];
 
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+      */
+    // protected $casts = [
+    //     'email_verified_at' => 'datetime',
+    // ];
 }
--- database/factories/OptionFactory.php
+++ database/factories/OptionFactory.php
@@ -8,7 +8,7 @@ use Faker\Generator as Faker;
 
 $factory->define(Option::class, function (Faker $faker) {
     return [
-        'question_id' => $faker->numberBetween(1,50),
+        'question_id' => $faker->numberBetween(51,100),
         'body' => $faker->text(),
         'isCorrect' => $faker->boolean(25),
     ];
