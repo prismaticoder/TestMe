460b5144638ea1f62716030715a255c8d856dd79
prismathic <pjjsalam1999@gmail.com>
2019-11-01 23:06:44 +0100

    configured view for host exam

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/singleresult', function() {
     return view('admin.main-result');
 });
 
+Route::get('/hostexam', function() {
+    return view('admin.host-exam');
+});
+
 Route::get('/login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('/login', 'Auth\LoginController@login');
 Route::get('/logout', 'Auth\LoginController@logout')->name('logout');
