e5c34351d7e30f5d74fb6cdb853f51a6eb1aef16
prismathic <pjjsalam1999@gmail.com>
2019-11-02 19:04:10 +0100

    Summernote will now reset upon question update

--- public/js/functions2.js
+++ public/js/functions2.js
@@ -119,7 +119,7 @@ $(function() {
                 console.log(response)
                 $('.list-group').append("<span class=\"questionBtn list-group-item list-group-item-action\" id=\""+response.question.id+"\">Question "+response.count+"</span>")
                 document.getElementById('myForm').reset();
-                $('#summernote').code("");
+                $('#summernote').summernote("reset");
             },
             error:function(response) {
                 console.log(response)
