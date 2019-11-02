e7be5eedc0612e259fe6df6a749c364324c342bc
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-02 17:45:04 +0100

    fix

--- resources/views/admin/host-exam.blade.php
+++ resources/views/admin/host-exam.blade.php
@@ -1,160 +1,175 @@
-{{-- The page that the admin sees after setting the exam modalities in the "Host Exam" modal. Basic Page to show the timer. The number of students who have submitted and also those who are currently undertaking the course --}}
-{{-- This is the view for the admin main page. It contains the list of all subjects with links to host exam, add questions, view results, it also has a navbar with links to the students page --}}
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="/docs/4.1/assets/img/favicons/favicon.ico">
-
-    <title>Admin Dashboard</title>
+    <title>Welcome Page</title>
 
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
 
-    <!-- Custom styles for this template -->
+
     <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+      /*
+        * Globals
+        */
+
+        /* Links */
+        a,
+        a:focus,
+        a:hover {
+        color: #fff;
+        }
+
+        /* Custom default button */
+        .btn-secondary,
+        .btn-secondary:hover,
+        .btn-secondary:focus {
+        color: #333;
+        text-shadow: none; /* Prevent inheritance from `body` */
+        background-color: #fff;
+        border: .05rem solid #fff;
+        }
+
+
+        /*
+        * Base structure
+        */
+
+        html,
         body {
-        font-size: .875rem;
+        height: 100%;
+        background-color: #333;
         }
 
-        .feather {
-        width: 16px;
-        height: 16px;
-        vertical-align: text-bottom;
+        body {
+        display: -ms-flexbox;
+        display: flex;
+        color: #fff;
+        text-shadow: 0 .05rem .1rem rgba(0, 0, 0, .5);
+        box-shadow: inset 0 0 5rem rgba(0, 0, 0, .5);
         }
 
-        .classes {
-          margin-top: 120px;
+        .cover-container {
+        max-width: 42em;
         }
-        .card-text {
-          font-size: 20px !important;
-          font-weight: bold;
+
+
+        /*
+        * Header
+        */
+        .masthead {
+        margin-bottom: 2rem;
+        }
+
+        .masthead-brand {
+        margin-bottom: 0;
+        }
+
+        .nav-masthead .nav-link {
+        padding: .25rem 0;
+        font-weight: 700;
+        color: rgba(255, 255, 255, .5);
+        background-color: transparent;
+        border-bottom: .25rem solid transparent;
+        }
+
+        .nav-masthead .nav-link:hover,
+        .nav-masthead .nav-link:focus {
+        border-bottom-color: rgba(255, 255, 255, .25);
+        }
+
+        .nav-masthead .nav-link + .nav-link {
+        margin-left: 1rem;
+        }
+
+        .nav-masthead .active {
+        color: #fff;
+        border-bottom-color: #fff;
+        }
+
+        @media (min-width: 48em) {
+        .masthead-brand {
+            float: left;
+        }
+        .nav-masthead {
+            float: right;
+        }
+        }
+
+
+        /*
+        * Cover
+        */
+        .cover {
+        padding: 0 1.5rem;
+        }
+        .cover .btn-lg {
+        padding: .75rem 1.25rem;
+        font-weight: 700;
+        }
+
+
+        /*
+        * Footer
+        */
+        .mastfoot {
+        color: rgba(255, 255, 255, .5);
         }
 
     </style>
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
   </head>
+  <body class="text-center">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="masthead mb-auto">
+    <div class="inner">
+      <h3 class="masthead-brand">OASIS-CBT ADMIN</h3>
+      <nav class="nav nav-masthead justify-content-center">
+        <a class="nav-link active" href="#">Home</a>
+        <a class="nav-link" href="#">Features</a>
+        <a class="nav-link" href="#">Contact</a>
+      </nav>
+    </div>
+  </header>
 
-  <body>
-  <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-    <a class="navbar-brand" href="{{route('dashboard')}}">OASIS-CBT ADMIN</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link active" href="{{route('dashboard')}}">Dashboard <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
-            </li>
-            </ul>
-            <span class="navbar-text">
-            <a class="nav-link" href="{{route('admin-logout')}}">Sign out</a>
-            </span>
-        </div>
-    </nav>
-
-    <div class="container">
-
-        <main role="main" class="classes">
-          <h1>Students</h1>
-          <div class="card-deck">
-                <div class="card">
-                    <div class="card-body">
-                      <h5 class="card-title">Junior Secondary School </h5>
-                      <p class="card-text">123 Students</P>
-                    </div>
-                    <div class="card-footer">
-                    <small class="text-muted"><a href="#">See Student List</a></small>
-                    </div>
-                </div>
-          </div>
-          </main>
-
-        <h1 style="margin-top:50px;">Subjects</h1>
-
-        <main class="card-columns">
-          <div class="card">
-            <div class="card-body">
-              <p class="card-text"></p>
-            </div>
-            <div class="card-footer">
-              <small class="text-muted">
-              <a href="#" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Start Exam</a>
-              <a href="#" class="btn btn-secondary">JSS 1</a>
-              <a href="#" class="btn btn-secondary">JSS 2</a>
-              <a href="#" class="btn btn-secondary">JSS 3</a>
-              </small>
-            </div>
-          </div>
-        </main>
-
-        </div>
-
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-              <form>
-                <div class="row">
-                <div class="col-sm-5">
-                    <label for="">Exam Duration</label>
-                  </div>
-                  <div class="col-sm-3">
-                    <input type="number" class="form-control" placeholder="Hour">
-                  </div>
-                  <div class="col-sm-3">
-                    <input type="number" class="form-control" placeholder="Minutes">
-                  </div>
-                </div>
-                <div class="form-group row mt-3">
-                  <label for="colFormLabel" class="col-sm-5 col-form-label">Total Allotable Marks</label>
-                  <div class="col-sm-4">
-                    <input type="number" class="form-control" id="colFormLabel" placeholder="score">
-                  </div>
-                </div>
-              </form>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Start Exam</button>
-              </div>
-            </div>
-          </div>
-        </div>
+  <main role="main" class="inner cover">
 
 
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
-    <script src="../../assets/js/vendor/popper.min.js"></script>
+    <h1 class="cover-heading">Literature Exam</h1><br>
+    <p class="lead">Instructions</p>
+    <p class="lead">The API endpoints should respond with a JSON object specifying the status of the operation, and
+either a data property (on success) or an error property (on failure). When present, the data
+property is always an object or an array</p>
+    <p class="lead">Time Countdown</p>
+    <h1 class="cover-heading">01:05</h1><br>
 
-    <script src="{{asset('js/jquery.js')}}"></script>
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <p class="lead">
+      <a href="#" class="btn btn-lg btn-secondary">End Exam</a>
+    </p>
+  </main>
 
-    <!-- Icons -->
-    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
-    <script>
-      feather.replace()
-    </script>
-
-  </body>
+  <footer class="mastfoot mt-auto">
+    <div class="inner">
+      <p>&copy;2019 OASIS-CBT ADMIN</p>
+    </div>
+  </footer>
+</div>
+</body>
 </html>
