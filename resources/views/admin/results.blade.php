205d60913028ce3a554af74e747e0d6c35a75ba5
Vick Greenfields <akintoluvic@users.noreply.github.com>
2019-11-02 18:03:00 +0100

    fix

--- resources/views/admin/results.blade.php
+++ resources/views/admin/results.blade.php
@@ -28,9 +28,6 @@
         vertical-align: text-bottom;
         }
 
-        .classes {
-          margin-top: 120px;
-        }
         .card-text {
           font-size: 20px !important;
           font-weight: bold;
@@ -67,12 +64,64 @@
 
         
 
-        <h1 style="margin-top:50px;">Subjects</h1>
+        <h1 style="margin-top:120px;">Exam Results Page</h1>
 
-        <main class="card-columns">
+        <main class="card-columns mt-3">
           <div class="card">
             <div class="card-body">
-              <p class="card-text"></p>
+              <p class="card-text">English Language</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">
+              Class results > 
+              <a href="#" class="btn btn-secondary">JSS 1</a>
+              <a href="#" class="btn btn-secondary">JSS 2</a>
+              <a href="#" class="btn btn-secondary">JSS 3</a>
+              </small>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <p class="card-text">English Language</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">
+              Class results > 
+              <a href="#" class="btn btn-secondary">JSS 1</a>
+              <a href="#" class="btn btn-secondary">JSS 2</a>
+              <a href="#" class="btn btn-secondary">JSS 3</a>
+              </small>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <p class="card-text">English Language</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">
+              Class results > 
+              <a href="#" class="btn btn-secondary">JSS 1</a>
+              <a href="#" class="btn btn-secondary">JSS 2</a>
+              <a href="#" class="btn btn-secondary">JSS 3</a>
+              </small>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <p class="card-text">English Language</p>
+            </div>
+            <div class="card-footer">
+              <small class="text-muted">
+              Class results > 
+              <a href="#" class="btn btn-secondary">JSS 1</a>
+              <a href="#" class="btn btn-secondary">JSS 2</a>
+              <a href="#" class="btn btn-secondary">JSS 3</a>
+              </small>
+            </div>
+          </div>
+          <div class="card">
+            <div class="card-body">
+              <p class="card-text">English Language</p>
             </div>
             <div class="card-footer">
               <small class="text-muted">
@@ -87,43 +136,6 @@
 
         </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-          <div class="modal-dialog modal-dialog-centered" role="document">
-            <div class="modal-content">
-              <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
-                </button>
-              </div>
-              <div class="modal-body">
-              <form>
-                <div class="row">
-                <div class="col-sm-5">
-                    <label for="">Exam Duration</label>
-                  </div>
-                  <div class="col-sm-3">
-                    <input type="number" class="form-control" placeholder="Hour">
-                  </div>
-                  <div class="col-sm-3">
-                    <input type="number" class="form-control" placeholder="Minutes">
-                  </div>
-                </div>
-                <div class="form-group row mt-3">
-                  <label for="colFormLabel" class="col-sm-5 col-form-label">Total Allotable Marks</label>
-                  <div class="col-sm-4">
-                    <input type="number" class="form-control" id="colFormLabel" placeholder="score">
-                  </div>
-                </div>
-              </form>
-              </div>
-              <div class="modal-footer">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Start Exam</button>
-              </div>
-            </div>
-          </div>
         </div>
 
 
