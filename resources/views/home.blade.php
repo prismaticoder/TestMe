f5393962288bccfc6fe6d3f6322c37b0dc9b4b1d
prismathic <pjjsalam1999@gmail.com>
2019-12-01 18:58:03 +0100

    Little FE fixes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,15 +42,12 @@
 
   <body>
   <nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">OASIS-CBT</a>
+    <a class="navbar-brand" href="#">OASIS ROYAL ACADEMY COMPUTER BASED TESTING (CBT) EXAMINATION</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link active" href="{{route('dashboard')}}">Dashboard <span class="sr-only">(current)</span></a>
-            </li>
             <li class="nav-item">
                 <a class="nav-link" href=""></a>
             </li>
