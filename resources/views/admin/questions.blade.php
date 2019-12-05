75e84d1a35610ae291800a5896d0e3c2def0262d
prismathic <pjjsalam1999@gmail.com>
2019-12-05 09:49:14 +0100

    Changed FE to load from CDN rather than public folder

--- resources/views/admin/questions.blade.php
+++ resources/views/admin/questions.blade.php
@@ -8,13 +8,16 @@
     <meta name="csrf-token" content="{{csrf_token()}}">
 <title>Oasis CBT | JSS{{$class_id}} {{$subject->subject_name}} Questions</title>
     <!-- include libraries(jQuery, bootstrap) -->
-    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
-    <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet">
+    {{-- <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet"> --}}
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    {{-- <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet"> --}}
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
     <script src="{{asset('js/popper.min.js')}}"></script>
-    <script src="{{asset('js/jquery.js')}}"></script>
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    {{-- <script src="{{asset('js/bootstrap.min.js')}}"></script> --}}
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script src="{{asset('/js/functions2.js')}}"></script>
-    {{-- <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css"> --}}
+    {{--  --}}
 
     <!-- include summernote css-->
     <link href="{{asset('css/summernote-bs4.css')}}" rel="stylesheet">
@@ -316,6 +319,7 @@
 
     </div>
     </div>
+
 
 
 
