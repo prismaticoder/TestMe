1c98cc27b7b7de4f154e9221b82c782958dc0cdd
prismathic <pjjsalam1999@gmail.com>
2020-07-28 13:08:59 +0100

    feat: done with subjects functionality, now to add fn for each class

---
+++ resources/js/components/admin/SingleClass.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row border-bottom">
+        <div class="col-12 text-center">
+            <span>{{single.class}}</span>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-5">
+            <v-btn :href="`/admin/subjects/${subject.alias}/${single.id}/questions`" small title="Go to Questions" :color="yellow">
+                Questions
+            </v-btn>
+        </div>
+        <div class="col-1"></div>
+        <div class="col-5">
+            <v-btn v-if="!single.startedExams[subject.subject_name]" :disabled="!single.examsWithParamsSet[subject.subject_name]" small :title="single.examsWithParamsSet[subject.subject_name] ? 'Start Exam' : 'You cannot start this exam because exam duration has not been set'" :color="yellow">
+                Begin Exam
+            </v-btn>
+            <v-btn v-else small tile title="End Exam" :color="yellow">
+                End Exam
+            </v-btn>
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "SingleClass",
+    props: ['single', 'subject', 'yellow']
+}
+</script>
+
+<style>
+
+</style>
---
+++ resources/js/components/admin/Subjects.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+      <v-expansion-panels accordion hover focusable>
+          <v-expansion-panel class="col-md-4" v-for="subject in subjects" :key="subject.id">
+                <v-expansion-panel-header><h5>{{subject.alias.toUpperCase()}}</h5></v-expansion-panel-header>
+                    <v-expansion-panel-content>
+                        <SingleClass :single="single" :subject="subject" :yellow="yellow" v-for="single in classes" :key="single.id" />
+                    </v-expansion-panel-content>
+          </v-expansion-panel>
+      </v-expansion-panels>
+  </div>
+</template>
+
+<script>
+import SingleClass from './SingleClass';
+export default {
+    name: "Subjects",
+    props: ['subjects', 'classes'],
+    data() {
+        return {
+            yellow: "#e67d23",
+            dark: "#343a40"
+        }
+    },
+    components: {
+        SingleClass
+    }
+}
+</script>
+
+<style scoped>
+.active {
+    background-color: #343a40;
+    color: #e67d23;
+    border: solid #343a40 1px;
+}
+</style>
