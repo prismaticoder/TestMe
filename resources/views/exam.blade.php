a961df3fab78c60f1e60f1fa8756a29352fef92c
prismathic <pjjsalam1999@gmail.com>
2020-09-07 23:20:19 +0100

    feat: done with making necessary modifications to exam page

--- resources/views/exam.blade.php
+++ resources/views/exam.blade.php
@@ -6,15 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{csrf_token()}}">
-<title>Oasis CBT | {{$subject->subject_name}} Examination</title>
+    <link rel="icon" href="{{asset('/img/logo.png')}}">
+
+
+    <title>{{config('app.name')}} | {{config('app.schoolAlias')}} - {{ucfirst($subject->subject_name). ' ' . Auth::user()->class->class}} Examination</title>
 
         <!-- Bootstrap core CSS -->
         <link rel="stylesheet" href="{{asset('/css/bootstrap.min.css')}}">
     <style>
-        html,
+        /* html,
         body {
         border: border-box;
-        }
+        } */
 
         body {
         background-color: #f5f5f5;
@@ -40,44 +43,59 @@
             background-color: #fff;
         }
 
+        .radios.disabled:hover {
+            cursor: not-allowed
+        }
         .radios:hover {
             border:solid #204d74 1px;
+        }
+
+        ul .nav-link:hover {
+            border-bottom: solid #e67d23 2px
+        }
+
+        ul>.nav-item>.active {
+            border-bottom: solid #e67d23 2px
+        }
+
+        ul .nav-link {
+            padding: 8px;
+            margin-left: 12px;
         }
     </style>
 
 </head>
-<body class="container-fluid">
-    <div id="app" data-app="true">
-<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
-<a class="navbar-brand navbar-text" style="color:white">{{config('app.schoolName')}}</a>
-<div class="order-5">
-    <Timer :hours="{{$hours}}" :minutes="{{$minutes}}"></Timer>
-</div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-            <a class="nav-link"><span>{{strtoupper($subject->subject_name)}}</span> JSS<span class="class">{{$user->class_id}}</span> <span class="sr-only">(current)</span></a>
-            </li>
-            <span style="display:none" class="subject">{{strtoupper($subject->alias)}}</span>
-            <li class="nav-item">
-            <a class="nav-link">{{$user->fullName}}</a>
-            </li>
-            <li class="nav-item">
-            <a class="nav-link">Exam No: {{$user->code}}</a>
-            </li>
-            </ul>
-            <input type="hidden" name="" class="reloader" value="0">
-        </div>
-    </nav>
+<body>
+    <div id="app">
+        <nav class="navbar navbar-expand-lg sticky-top navbar-dark bg-dark">
+            <a href="">
+                <img src="{{asset('/img/logo.png')}}" class="mr-2" height="45" width="45">
+            </a>
+            <a class="navbar-brand" href="">
+                {{config('app.name')}} | {{config('app.schoolAlias')}} {{strtoupper($subject->subject_name) . ' ' . Auth::user()->class->class}} EXAMINATION
+            </a>
+            <div class="order-5">
+                <Timer :hours="{{$hours}}" :minutes="{{$minutes}}"></Timer>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mx-auto col-md-5">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="">{{Auth::user()->fullName}} (Exam No {{Auth::user()->code}})</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-    <div>
-        {{-- made it class-id(kebab-case) because HTML converts it to lowercase automatically --}}
-    <Questions :questions="{{$questions}}" :hours="{{$hours}}" :minutes="{{$minutes}}" :subject="{{$subject->id}}" :class-id="{{$user->class_id}}"></Questions>
+        <div>
+            {{-- made it class-id(kebab-case) because HTML converts it to lowercase automatically --}}
+            <v-app>
+            <Questions :questions="{{$questions}}" :hours="{{$hours}}" :minutes="{{$minutes}}" :mainsubject="{{$subject}}" :subject="{{$subject->id}}" :class-id="{{$user->class_id}}"></Questions>
+            </v-app>
+        </div>
     </div>
-</div>
 <script src="{{asset('/js/app.js')}}"></script>
 
 <script src="{{asset('/js/jquery.js')}}"></script>
