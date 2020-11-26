1b4ea61c7ead53bc314293d1cb029afc70953cbb
prismathic <pjjsalam1999@gmail.com>
2020-11-26 19:30:27 +0100

    fix: necessary changes to migration to avoid breaking on prod

--- database/migrations/2019_10_23_091107_create_options_table.php
+++ database/migrations/2019_10_23_091107_create_options_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAnswersTable extends Migration
+class CreateOptionsTable extends Migration
 {
     /**
      * Run the migrations.
