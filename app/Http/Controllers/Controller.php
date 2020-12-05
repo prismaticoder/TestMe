82f3286c4e4543794d7bac93c71202267f3f4568
prismathic <pjjsalam1999@gmail.com>
2020-12-05 18:47:01 +0100

    add methods for formatting API response either success or error

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,9 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function sendSuccessResponse(string $message, ?array $data = [], int $status = 200): JsonResponse
+    {
+        return response()->json(array(
+            'success' => true,
+            'message' => $message,
+            'data' => $data
+        ), $status);
+    }
+
+    protected function sendErrorResponse(string $message, int $status = 500): JsonResponse
+    {
+        return response()->json(array(
+            'success' => false,
+            'message' => $message,
+        ), $status);
+    }
 }
