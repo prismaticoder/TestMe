1d0a3723d771e328cb837119074319bcc3951ec4
prismathic <pjjsalam1999@gmail.com>
2020-12-08 16:24:33 +0100

    add request classes for teacher resource

---
+++ app/Http/Requests/CreateTeacherRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTeacherRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'username' => ['required', 'string', 'unique:admins', 'max:255'],
+            'password' => ['required', 'string'],
+            'subjects' => ['required', 'array'],
+            'subjects.*.subject_id' => ['required', 'exists:subjects,id'],
+            'subjects.*.classes' => ['required', 'array'],
+            'subjects.*.classes.*' => ['required', 'exists:classes,id']
+        ];
+    }
+}
