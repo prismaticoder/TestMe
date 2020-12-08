959eb87db803efce4cca1ff64fd7d7220c69dd7b
prismathic <pjjsalam1999@gmail.com>
2020-12-08 17:03:11 +0100

    refactor checkexamstatus middleware class

--- app/Http/Middleware/CheckExamStatus.php
+++ app/Http/Middleware/CheckExamStatus.php
@@ -4,7 +4,6 @@ namespace App\Http\Middleware;
 
 use App\Exam;
 use Closure;
-use Illuminate\Support\Facades\Session;
 
 class CheckExamStatus
 {
@@ -17,22 +16,12 @@ class CheckExamStatus
      */
     public function handle($request, Closure $next)
     {
-        $exam_id = Session::get('exam_id');
+        $exam_id = session()->get('exam_id');
 
         $exam = Exam::find($exam_id);
 
-        if ($exam) {
-            if ($exam->hasStarted) {
-                return abort(403, "Error: this action cannot be performed for an examination in progress");
-            }
+        abort_if($exam && $exam->has_started, 403, "Error: this action cannot be performed for an examination in progress");
 
-            else {
-                return $next($request);
-            }
-        }
-
-        else {
-            return $next($request);
-        }
+        return $next($request);
     }
 }
