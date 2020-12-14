b6959bd110abeab238433ac56428da42faed8ece
prismathic <pjjsalam1999@gmail.com>
2020-12-14 16:48:16 +0100

    removed unnecessary features based on refactor

--- app/Classes.php
+++ app/Classes.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use App\Mark;
 
 class Classes extends Model
 {
@@ -16,36 +15,5 @@ class Classes extends Model
     public function subjects() {
         return $this->belongsToMany(Subject::class, 'class_subject','class_id');
     }
-
-    public function admins() {
-        return $this->belongsToMany(Admin::class);
-    }
-
-    public function hasStarted($subject_id) {
-        //check if a specific class has started exams for a specific subject
-        $mark = Mark::where('subject_id',$subject_id)->where('class_id', $this->id)->first();
-        return  $mark ? $mark->hasStarted : 0;
-    }
-
-    public function hasPendingExamToday($subject_id) {
-        //check if a specific class has started exams for a specific subject
-        $exam = Exam::where('subject_id',$subject_id)->where('class_id',$this->id)->orderBy('date','desc')->orderBy('updated_at','desc')->first();
-
-        if (!$exam) {
-            return false;
-        }
-        else {
-            //only return true for subjects that have questions
-            return $exam->date == date('Y-m-d') && count($exam->questions) > 0;
-        }
-    }
-
-    public function getAllExams($subject_id) {
-        $exams = Exam::where('subject_id',$subject_id)->where('class_id',$this->id)->orderBy('date','desc')->orderBy('updated_at','desc')->get();
-
-        return $exams;
-    }
-
-
 }
 
