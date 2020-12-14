a8cfe1897a0a17fefdf92a257fae146b1bb15d4d
prismathic <pjjsalam1999@gmail.com>
2020-12-14 16:55:06 +0100

    fixes

--- app/TeacherSubject.php
+++ app/TeacherSubject.php
@@ -8,7 +8,7 @@ class TeacherSubject extends Model
 {
     protected $table = "teacher_subject";
     protected $fillable = ['teacher_id', 'subject_id'];
-    protected $appends = ['subject_name','alias'];
+    protected $appends = ['name','alias'];
     public $timestamps = false;
 
     public function subject() {
@@ -23,8 +23,8 @@ class TeacherSubject extends Model
         return $this->belongsToMany(Classes::class, 'teachersubject_class', 'teachersubject_id', 'class_id');
     }
 
-    public function getSubjectNameAttribute() {
-        return $this->subject->subject_name;
+    public function getNameAttribute() {
+        return $this->subject->name;
     }
 
     public function getAliasAttribute() {
