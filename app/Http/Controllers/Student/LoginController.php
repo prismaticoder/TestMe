e78e50ce2bb6a7c631f9d3ace31873d836f42b49
prismathic <pjjsalam1999@gmail.com>
2020-12-14 16:56:57 +0100

    added login and submission functions

---
+++ app/Http/Controllers/Student/LoginController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Http\Controllers\Controller;
+use App\Student;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    public function __construct()
+    {
+        $this->middleware('auth')->only('logout');
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function index()
+    {
+        return view('auth.login');
+    }
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $user = Student::where('examination_number', $request->examination_number)->first();
+
+        if (! $user) {
+            return redirect('login')->withErrors('Incorrect Login Credentials');
+        }
+
+        auth()->login($user);
+        return redirect()->intended('/home');
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    protected function validateLogin(Request $request)
+    {
+        $request->validate([
+            'examination_number' => 'required|string|size:6|alpha_num',
+        ]);
+    }
+
+    public function logout() {
+        auth()->logout();
+        return redirect('/login');
+    }
+
+}
---
+++ app/Http/Controllers/Student/SubmissionsController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Student;
+
+use App\Exam;
+use App\Http\Controllers\Controller;
+use App\Question;
+use Illuminate\Http\Request;
+
+class SubmissionsController extends Controller
+{
+
+    /**
+     * Create a new submission
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'choices' => ['required', 'json']
+        ]);
+
+        $examId = session()->get('exam_id');
+
+        abort_if(auth()->user()->submissions->where('exam_id', $examId)->exists(), 403, "You have already made a submission for this exam");
+
+        $choices = json_decode($request->choices);
+        $scores = $this->computeExamScore($choices);
+
+        auth()->user()->submissions()->create([
+            'exam_id' => $examId,
+            'actual_score' => $scores['actual'],
+            'computed_score' => $scores['computed']
+        ]);
+
+        return $this->sendSuccessResponse("Submission successful!");
+    }
+
+    private function computeExamScore(array $studentResponses): array
+    {
+        $examId = session()->get('exam_id');
+        $questions = Question::where(compact('exam_id'))->with('options')->inRandomOrder(auth()->user()->seed)->get();
+        $baseScore = Exam::find($examId)->base_score;
+        $averagePoint = $baseScore/count($questions);
+
+        $score = 0;
+        foreach ($studentResponses as $response) {
+            $correspondingQuestion = $questions[$response->question - 1];
+            if (! is_null($response->choice)) {
+                $score += (int) $correspondingQuestion->options[$response->choice]->is_correct;
+            }
+        }
+
+        return array(
+            'actual' => $score,
+            'computed' => $score * $averagePoint
+        );
+    }
+}
