fb320b4dafabf77a684a6953dcd415d524d94fb0
prismathic <pjjsalam1999@gmail.com>
2021-01-04 23:37:14 +0100

    changes to ensure choices array is validated

--- app/Http/Controllers/Student/SubmissionsController.php
+++ app/Http/Controllers/Student/SubmissionsController.php
@@ -19,15 +19,17 @@ class SubmissionsController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'choices' => ['required', 'json']
+            'choices' => ['required', 'array'],
+            'choices.*' => ['required', 'array', 'size:2'],
+            'choices.*.question' => ['required', 'int', 'not_in:0'],
+            'choices.*.choice' => ['nullable', 'int', 'digits_between:0,3']
         ]);
 
         $examId = session()->get('exam_id');
 
-        abort_if(auth()->user()->submissions->where('exam_id', $examId)->exists(), 403, "You have already made a submission for this exam");
+        abort_if(auth()->user()->submissions()->where('exam_id', $examId)->exists(), 403, "You have already made a submission for this exam");
 
-        $choices = json_decode($request->choices);
-        $scores = $this->computeExamScore($choices);
+        $scores = $this->computeExamScore($request->choices);
 
         auth()->user()->submissions()->create([
             'exam_id' => $examId,
@@ -41,17 +43,21 @@ class SubmissionsController extends Controller
     private function computeExamScore(array $studentResponses): array
     {
         $examId = session()->get('exam_id');
-        $questions = Question::where(compact('exam_id'))->with('options')->inRandomOrder(auth()->user()->seed)->get();
+        $questions = Question::where('exam_id', $examId)->with('options')->inRandomOrder(auth()->user()->seed)->get();
         $baseScore = Exam::find($examId)->base_score;
         $averagePoint = $baseScore/$questions->count();
 
-        $score = 0;
-        foreach ($studentResponses as $response) {
-            $correspondingQuestion = $questions[$response->question - 1];
-            if (! is_null($response->choice)) {
-                $score += (int) $correspondingQuestion->options[$response->choice]->is_correct;
+        $score = collect($studentResponses)->reduce(function ($currentScore, $response) use($questions) {
+
+            if (is_null($response['choice'])) {
+                return $currentScore;
             }
-        }
+
+            $correspondingQuestion = $questions[$response['question'] - 1];
+            $chosenOption = $correspondingQuestion->options[$response['choice']];
+
+            return $currentScore + (int)$chosenOption->is_correct ?? 0;
+        }, 0);
 
         return array(
             'actual' => $score,
